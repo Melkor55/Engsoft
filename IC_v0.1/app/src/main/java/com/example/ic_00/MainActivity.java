6f39571d4ea76d8159256b92e4d202eb1e0e654f
Melkor55 <melkor2500@gmail.com>
2022-05-10 19:21:41 +0300

    Adding a few interface files

---
+++ IC_v0.1/app/src/main/java/com/example/ic_00/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ic_00;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button LoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LoginButton = (Button) findViewById(R.id.login_button);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
