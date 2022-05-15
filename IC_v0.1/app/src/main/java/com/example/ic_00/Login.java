8d560659f23529002a6eb57e347aec1a71d5839b
Melkor55 <melkor2500@gmail.com>
2022-05-15 06:20:14 +0300

    Working Register and Login activities added -linked to a basic MySQL database(not online yet)

--- IC_v0.1/app/src/main/java/com/example/ic_00/Login.java
+++ IC_v0.1/app/src/main/java/com/example/ic_00/Login.java
@@ -1,8 +1,7 @@
 package com.example.ic_00;
-import java.io.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-//import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,68 +13,55 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
-public class Register extends AppCompatActivity {
-
-    EditText textInputEditTextName, textInputEditTextUsername, textInputEditTextEmail, textInputEditTextPassword;
-    Button registerButton;
+public class Login extends AppCompatActivity {
+    EditText textInputEditTextUsername, textInputEditTextPassword;
+    Button loginButton;
     TextView textViewLogin;
     ProgressBar progressBar;
-    private Button loginButton;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_login);
 
-        textInputEditTextName = (EditText) findViewById(R.id.register_nameField);
-        textInputEditTextUsername = (EditText) findViewById(R.id.register_usernameField);
-        textInputEditTextEmail = (EditText) findViewById(R.id.register_emailField);
-        textInputEditTextPassword = (EditText) findViewById(R.id.register_passwordField);
+        textInputEditTextUsername = (EditText) findViewById(R.id.login_usernameField);
+        textInputEditTextPassword = (EditText) findViewById(R.id.login_passwordField);
         loginButton = (Button) findViewById(R.id.login_button);
-        registerButton  = findViewById(R.id.register_button_stage2);
-        progressBar = findViewById(R.id.register_progressBar);
-        progressBar.setVisibility(View.INVISIBLE);
+        //progressBar = findViewById(R.id.register_progressBar);
+        //progressBar.setVisibility(View.INVISIBLE);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                String name,username,email,password;
-                name = String.valueOf(textInputEditTextName.getText());
+                String username,password;
                 username = String.valueOf(textInputEditTextUsername.getText());
-                email = String.valueOf(textInputEditTextEmail.getText());
                 password = String.valueOf(textInputEditTextPassword.getText());
 
 
-                if( !name.equals("") && !username.equals("") && !email.equals("") && !password.equals("") )
+                if( !username.equals("") && !password.equals("") )
                 {
                     //Start ProgressBar first (Set visibility VISIBLE)
-                    progressBar.setVisibility(View.VISIBLE);
                     for(int i = 0 ; i < 10000 ; i ++);
                     Handler handler = new Handler(Looper.getMainLooper());
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-                            System.out.println(name);
                             //Starting Write and Read data with URL
                             //Creating array for parameters
-                            String[] field = new String[4];
-                            field[0] = "name";
-                            field[1] = "username";
-                            field[2] = "email";
-                            field[3] = "password";
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
 
                             //Creating array for data
-                            String[] data = new String[4];
-
-                            data[0] = name;
-                            data[1] = username;
-                            data[2] = email;
-                            data[3] = password;
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
                         /*
                         data[0] = "ana";
                         data[1] = "anaa";
@@ -83,20 +69,19 @@ public class Register extends AppCompatActivity {
                         data[3] = "1234";
                          */
                             //  url : http://current_ip_adress(ip_config)/the_folder_where_signup.php_is_stored/signup.php
-                            PutData putData = new PutData("http://192.168.1.8/LoginRegister/signup.php", "POST", field, data);
+                            PutData putData = new PutData("http://192.168.1.8/LoginRegister/login.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
-                                    progressBar.setVisibility(View.GONE);
                                     String result = putData.getResult();
                                     System.out.println("--->" + result);
                                     //End ProgressBar (Set visibility to GONE)
-                                    if (result.equals("Sign Up Success")) {
+                                    if (result.equals("Login Success")) {
                                         System.out.println("[31m Good job!");
                                         //Intent intent = new Intent(getApplicationContext(),)
 
                                         Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
 
-                                        Intent intent = new Intent(Register.this, MainActivity.class);
+                                        Intent intent = new Intent(Login.this, HomePage.class);
                                         startActivity(intent);
                                     }
                                     else {
@@ -122,9 +107,5 @@ public class Register extends AppCompatActivity {
                  */
             }
         });
-
-
-
-
     }
 }
--- IC_v0.1/app/src/main/java/com/example/ic_00/MainActivity.java
+++ IC_v0.1/app/src/main/java/com/example/ic_00/MainActivity.java
@@ -11,19 +11,30 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button LoginButton;
+    private Button LoginButton,RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LoginButton = (Button) findViewById(R.id.register_button);
-        LoginButton.setOnClickListener(new View.OnClickListener() {
+        RegisterButton = (Button) findViewById(R.id.registerButton);
+        LoginButton = (Button) findViewById(R.id.loginButton);
+
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
                 Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(MainActivity.this, Login.class);
                 startActivity(intent);
             }
         });
